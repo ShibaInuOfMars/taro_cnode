0b589cd62fdc0db0904ff7a6034513fd192d4f15
ShibaInuOfMars <1051858676@qq.com>
2019-11-04 01:15:07 +0800

    添加加载提示

--- src/utils/tAjax.js
+++ src/utils/tAjax.js
@@ -5,26 +5,32 @@ export default (url, data, method = 'GET') => {
   data = data || {}
   return new Promise((resolve) => {
     if(method === 'GET') {
+      Taro.showLoading({title: '加载中...'})
       return Taro.request({
         url,
         data,
         method
       }).then(res => {
+        Taro.hideLoading()
         resolve(res.data);
       }).catch(() => {
+        Taro.hideLoading()
         Taro.showToast({
           title: '网络异常，请稍后再试',
           icon: 'none'
         })
       })
     } else if (method === 'POST') {
+      Taro.showLoading({title: '加载中...'})
       return Taro.request({
         url,
         data,
         method
       }).then(res => {
+        Taro.hideLoading()
         resolve(res.data);
       }).catch(() => {
+        Taro.hideLoading()
         Taro.showToast({
           title: '网络异常，请稍后再试',
           icon: 'none'
